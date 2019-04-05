6c9efe4f67915e699898309dea296eddeda3c2c0
Alexander Mendelsohn <superparkourio@gmail.com>
2019-04-04 22:09:29 -0400

    Generated model events.js properly

--- models/events.js
+++ models/events.js
@@ -1,40 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Events = sequelize.define(
-    'Events',
-    {
-      name:  {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      startTime: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      endTime: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    },
-    {
-      classMethods: {
-        associate: function(models) {
-          // associations can be defined here
-          models.Events.belongsTo(models.Users, {
-            foreignKey: 'userId',
-            sourceKey: 'id',
-          });
-        },
-      },
-    },
-  );
-  return Events;
+  const events = sequelize.define('events', {
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    startTime: DataTypes.DATE,
+    endTime: DataTypes.DATE,
+    userId: DataTypes.INTEGER
+  }, {});
+  events.associate = function(models) {
+    // associations can be defined here
+  };
+  return events;
 };
